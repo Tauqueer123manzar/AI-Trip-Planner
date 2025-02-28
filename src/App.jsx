13948877c4dd9fd0089a3b4660d180be1cf77646
Tauqueer Manzar <mdtauqueermanzar@gmail.com>
2025-03-01 03:35:16 +0530

    second commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
-import React from 'react'
-import { BrowserRouter as Router,Routes,Route } from 'react-router-dom'
-import Navbar from './components/Navbar'
+import React from 'react';
+import { BrowserRouter as Router,Routes,Route } from 'react-router-dom';
+import Navbar from './components/Navbar';
+import Home from './pages/Home';
 const App = () => {
   return (
    <>
     <Router>
     <Navbar/>
       <Routes>
+      <Route path="/" element={<Home/>}/>
       </Routes>
     </Router>
    </>
--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -2,9 +2,12 @@ import React from 'react'
 
 const Home = () => {
   return (
-    <div>
-      
+   <>
+    <div className='container mx-auto p-4 text-center mt-8'>
+       <h1 className='text-4xl font-bold text-red-700'>Welcome to Trip Planner</h1>
+        <p className='text-lg text-gray-600 mt-4'>Plan your trips with ease</p>
     </div>
+   </>
   )
 }
 
